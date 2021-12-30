a68379a2eae48ab16522f75537329d208234537f
Calvin Ong <calvinxhen@gmail.com>
2021-12-30 19:11:31 +0800

    Update intro and socialbar

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,15 +3,27 @@
 
 <template lang="pug">
 section
-    p(class="font-mono") Intro here, Good day
+  .introduction
+    p.font-mono Good Day to you, my name is
+    p.name.font-color-cloud Calvin#[br]Ong Wei Xhen
+    p I'm a frontend web developer that loves to build things that go swish swoosh
 </template>
 
 <style lang="sass" scoped>
+@import '@styles/variables'
+
 section
-    height: 100vh
-    width: 100%
-    display: flex
-    flex-flow: row wrap
-    justify-content: center
-    align-items: center
+  height: 100vh
+  display: flex
+  flex-flow: row wrap
+  justify-content: flex-start
+  align-items: center
+
+p
+  display: block
+  margin: 16px 0
+
+.name
+  font-size: 72px
+  margin-left: -3px
 </style>
--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -12,30 +12,74 @@ makeCompatible(LinkedinIcon)
 <template lang="pug">
 ul#social-bar
   li
-    button
-      codepen-icon(size="24")
+    a(href="https://codepen.io/owx940810/pens/popular" target="_blank")
+      codepen-icon.target(size="24")
+      codepen-icon.ghost(size="24")
   li
-    button
-      github-icon(size="24")
-  li 
-    button
-      facebook-icon(size="24")
-  li 
-    button
-      instagram-icon(size="24")
-  li 
-    button
-      linkedin-icon(size="24")
+    a(href="https://github.com/owx940810/" target="_blank")
+      github-icon.target(size="24")
+      github-icon.ghost(size="24")
+  li
+    a(href="https://www.facebook.com/calvin.xhen" target="_blank")
+      facebook-icon.target(size="24")
+      facebook-icon.ghost(size="24")
+  li
+    a(href="https://www.instagram.com/calvin_wx/" target="_blank")
+      instagram-icon.target(size="24")
+      instagram-icon.ghost(size="24")
+  li
+    a(href="https://www.linkedin.com/in/owx940810/" target="_blank")
+      linkedin-icon.target(size="24")
+      linkedin-icon.ghost(size="24")
 
 </template>
 
-<style lang="sass">
+<style lang="sass" scoped>
+@import '@styles/variables'
+
 #social-bar
   position: fixed
-  bottom: 0
-  left: 20px
+  bottom: 10vh
+  left: 6%
   display: flex
   flex-flow: column
   list-style-type: none
   margin: 0
+  padding: 0 0 10px 0
+
+  &::after
+    content: ""
+    position: absolute
+    top: 100%
+    left: 50%
+    border-left: 1px solid $grey
+    height: 10vh
+
+li
+  margin: 4px 0
+
+a
+  display: block
+  position: relative
+
+  &:hover
+    .ghost
+      opacity: 1
+      transform: translate(-2px, -1px)
+
+    .target
+      transform: translate(2px, 1px)
+      opacity: 0.5
+
+svg
+  stroke: $grey
+  transition: all $speed-fast ease-in-out
+
+.ghost
+  opacity: 0
+  position: absolute
+  top: 0
+  left: 0
+  stroke: $blue
+
 </style>
